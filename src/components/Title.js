2d484919ee5f8e72d5db6512a3ab23b09a5de9ea
Donald Parminter Lokong <don.parminter@gmail.com>
2018-04-06 19:37:44 +0300

    #XD

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = () => (
+	<div>
+		<h1 className="title-container__title">Weather Locator</h1>
+		<p className="title-container__subtitle">Find out localized weather info...</p>
+	</div>
+);
+
+export default Title;
